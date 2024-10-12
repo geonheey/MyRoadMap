3b5cb519c282e4cb85e5282c0cbc3db21beb7492
geonheey <ghyjas0704@gmail.com>
2024-10-12 23:45:31 +0900

    [Style] : MyMyTypography 생성

--- app/src/main/java/com/example/myroadmap/ui/theme/Theme.kt
+++ app/src/main/java/com/example/myroadmap/ui/theme/Theme.kt
@@ -35,7 +35,6 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun MyRoadMapTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
@@ -51,7 +50,7 @@ fun MyRoadMapTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
+        typography = MyTypography,
         content = content
     )
 }
