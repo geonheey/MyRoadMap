1af0b7dd946b6cf6eb9f9328ab8460ba71b1c285
geonheey <ghyjas0704@gmail.com>
2024-10-13 17:28:56 +0900

    [Style] : xml 파일 삭제 -> Color.kt로 색상 코드 이동 및 추가

--- app/src/main/java/com/example/myroadmap/ui/theme/Color.kt
+++ app/src/main/java/com/example/myroadmap/ui/theme/Color.kt
@@ -10,3 +10,15 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+//경로 시간/거리 정보 색상
+val BlueBox = Color(0xFF04243A)
+val YelloText = Color(0xFFFDD567)
+
+//도로 상태 색상
+val RouteUnknown = Color(0xFFE67F7E79)
+val RouteBlock = Color(0xFFFD382A)
+val RouteJam = Color(0xFFFD382A)
+val RouteDelay = Color(0xFFF7AB17)
+val RouteSlow = Color(0xFFF0D300)
+val RouteNormal = Color(0xFF3BD72C)
+val StrokeColor = Color(0xFFFFFFFF)
